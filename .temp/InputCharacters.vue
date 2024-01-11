d341118e3f7e31ab038f3169d38792f69799c1d0
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2024-01-11 13:59:22 +0100

    :adhesive_bandage: Change on npm scripts 'build' and with ignore files

---
+++ .temp/InputCharacters.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: Number,
+        default: 1,
+    },
+    checked: {
+        type: Boolean,
+        default: false,
+    },
+    list: {
+        type: Array,
+        default: () => {},
+    },
+    valuename: {
+        type: String,
+        required: true,
+    },
+    index: {
+        type: Number,
+        required: true,
+    }
+});
+defineEmits(["update:checked", "update:value"]);
+</script>
+
+<template>
+    <td v-if="type === 'checkbox'">
+        <input
+            class="checkbox"
+            type="checkbox"
+            :checked="checked"
+            @change="$emit('update:checked', index, valuename, $event.target.checked)"
+        >
+    </td>
+    <td v-if="type === 'select'">
+        <select :value="value" class="select" @change="$emit('update:value', index, valuename, $event.target.value)">
+            <option v-for="lvl in list" :key="lvl" :value="lvl">
+                {{ lvl }}
+            </option>
+            {{ value }}
+        </select>
+    </td>
+</template>
